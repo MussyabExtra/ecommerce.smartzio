88fc1e9d2cd1d801e4af10261ec758e87087bfd8
ShahrukhAlam01 <shahrukhalam286@gmail.com>
2025-06-17 23:58:48 +0500

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Navbar from './component/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      HELLOfdwsfewsgfewssgewgsgg
-    </>
+   <div className='w-full bg-zinc-900 text-white overflow-hidden'>
+    <Navbar/>
+
+   </div>
   )
 }
 
---
+++ src/component/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='w-full bg-zinc-900 text-white overflow-hidden'>
+        <h6 class="text-white font-medium">Refer a friend &amp; get $50 in credits each 🎉</h6>   
+    </div>
+  )
+}
+
+export default Navbar
